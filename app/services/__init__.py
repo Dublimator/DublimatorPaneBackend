721e1a530a8efe302cb198f7423ff62730798c89
Dublimator <dublimator@gmail.com>
2025-04-08 20:20:20 +0300

    remove old function

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,6 @@
 # app/services/__init__.py
 from .metrics_collector import get_container_metrics, get_system_metrics, analyze_metrics
-from .network_analyzer import analyze_network, detect_dos
+from .network_analyzer import analyze_network
 
 # Экспортируем все сервисы для удобного импорта
 
@@ -9,5 +9,4 @@ __all__ = [
     "get_system_metrics",
     "analyze_metrics",
     "analyze_network",
-    "detect_dos",
 ]
