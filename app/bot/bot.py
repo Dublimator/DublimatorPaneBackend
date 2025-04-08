39d82267098348f1af58e0afc5351aae92b52b09
Dublimator <dublimator@gmail.com>
2025-04-08 20:17:39 +0300

    update message dos

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from telegram import Bot, Update
 from telegram.error import TelegramError
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -25,17 +27,28 @@ async def send_alert(message: str) -> None:
     except TelegramError as e:
         logger.error(f"Ошибка при отправке уведомления: {e}")
 
-async def notify_dos_attack(incident: dict) -> None:
+async def notify_dos_attack(incidents: []) -> None:
     """
     Отправляет уведомление о DOS-атаке.
     """
     if settings.notifications.dos.condition:
-        message = (
-            "⚠️ Обнаружена DOS-атака!\n"
-            f"IP: {incident['ip']}\n"
-            f"Запросов в секунду: {incident['requests_per_second']}\n"
-            f"Время: {incident['timestamp']}"
-        )
+        message = f"⚠️ Обнаружена атака \n"
+
+        for incident in incidents:
+            time_start = datetime.fromtimestamp(incident["timeStart"]).strftime("%d.%m.%Y %H:%M:%S")
+
+            # Формируем строку message
+            message = message + (
+                f"------------------------\n"
+                f"Тип атаки: {incident['type']}\n"
+                f"IP-адрес: {incident['sourceIp']}\n"
+                f"Количество пакетов: {incident['count']}\n"
+                f"Время начала: {time_start}\n"
+                f"Статус: {'Активен' if incident['status'] else 'Завершён'}\n"
+                f"------------------------\n"
+            )
+
+
         await send_alert(message)
 
 async def notify_container_stopped(container_name: str) -> None:
@@ -89,6 +102,6 @@ async def start_bot() -> None:
         application.add_handler(CommandHandler("getChatId", get_chat_id))
 
         # Запускаем бота
-        logger.info("Бот запущен и ожидает сообщений...")
+        logger.info("Бот запущен")
     except Exception as e:
         logger.error(f"Ошибка при запуске бота: {e}")
