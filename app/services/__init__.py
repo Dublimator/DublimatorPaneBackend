bb23d20bc6005f48da69d34d4d4011958c305568
Dublimator <dublimator@gmail.com>
2025-04-12 02:21:08 +0300

    fix time requests, add uptime container

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,5 @@
 # app/services/__init__.py
-from .metrics_collector import get_container_metrics, get_system_metrics, analyze_metrics
+from .metrics_collector import get_container_metrics, get_system_metrics, analyze_metrics, get_latest_system_metrics, get_latest_docker_metrics
 from .network_analyzer import analyze_network
 
 # Экспортируем все сервисы для удобного импорта
@@ -9,4 +9,6 @@ __all__ = [
     "get_system_metrics",
     "analyze_metrics",
     "analyze_network",
+    "get_latest_system_metrics",
+    "get_latest_docker_metrics"
 ]
